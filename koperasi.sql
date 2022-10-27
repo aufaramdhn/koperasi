f22eaf47b96f8ad47cf6b638c95674c25bd91332
aufaramdhn <ramadhanaufa3@gmail.com>
2022-10-27 16:50:05 +0700

    update

--- koperasi.sql
+++ koperasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Okt 2022 pada 11.54
+-- Waktu pembuatan: 27 Okt 2022 pada 11.49
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 8.1.10
 
@@ -24,6 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `konfirmasi_pinjam`
+--
+
+CREATE TABLE `konfirmasi_pinjam` (
+  `id_konfirmasi_pinjam` int(11) NOT NULL,
+  `id_pinjam` int(11) NOT NULL,
+  `tgl_konfirmasi` datetime NOT NULL,
+  `expired` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `konfirmasi_pinjam`
+--
+
+INSERT INTO `konfirmasi_pinjam` (`id_konfirmasi_pinjam`, `id_pinjam`, `tgl_konfirmasi`, `expired`) VALUES
+(1, 1, '2022-10-23 14:08:45', '2022-10-24 09:08:45');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_pengembalian`
+--
+
+CREATE TABLE `tbl_pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `id_konfirmasi_peminjaman` int(11) NOT NULL,
+  `jumlah_pengembalian` bigint(20) NOT NULL,
+  `tgl_pengembalian` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tbl_pinjam`
 --
 
@@ -31,7 +64,6 @@ CREATE TABLE `tbl_pinjam` (
   `id_pinjam` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `jumlah_pinjam` varchar(100) NOT NULL,
-  `tempo_pinjam` varchar(11) NOT NULL,
   `tgl_pinjam` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `status` varchar(100) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,14 +72,14 @@ CREATE TABLE `tbl_pinjam` (
 -- Dumping data untuk tabel `tbl_pinjam`
 --
 
-INSERT INTO `tbl_pinjam` (`id_pinjam`, `id_user`, `jumlah_pinjam`, `tempo_pinjam`, `tgl_pinjam`, `status`) VALUES
-(1, 1, '400000', '', '2022-10-24 06:11:59', 'pending'),
-(2, 1, '500000', '', '2022-10-24 06:11:59', 'pending'),
-(3, 0, '400000', '', '2022-10-24 06:13:18', 'pending'),
-(4, 0, '400000', '', '2022-10-24 06:13:18', 'pending'),
-(5, 1, '1500000', '', '2022-10-25 08:20:16', 'pending'),
-(6, 1, '1250000', '', '2022-10-25 08:20:47', 'pending'),
-(7, 1, '1240000', '1 Bln', '2022-10-26 04:38:50', 'pending');
+INSERT INTO `tbl_pinjam` (`id_pinjam`, `id_user`, `jumlah_pinjam`, `tgl_pinjam`, `status`) VALUES
+(1, 1, '400000', '2022-10-24 06:11:59', 'pending'),
+(2, 1, '500000', '2022-10-24 06:11:59', 'pending'),
+(3, 0, '400000', '2022-10-24 06:13:18', 'pending'),
+(4, 0, '400000', '2022-10-24 06:13:18', 'pending'),
+(5, 1, '1500000', '2022-10-25 08:20:16', 'pending'),
+(6, 1, '1250000', '2022-10-25 08:20:47', 'pending'),
+(7, 1, '1240000', '2022-10-26 04:38:50', 'pending');
 
 -- --------------------------------------------------------
 
@@ -78,7 +110,8 @@ INSERT INTO `tbl_simpan` (`id_simpan`, `id_user`, `jumlah_simpan`, `tgl_simpan`)
 (9, 1, 4500000, '2022-09-01 00:39:16'),
 (10, 1, 5000000, '2022-10-01 00:39:16'),
 (11, 1, 5500000, '2022-11-01 00:40:05'),
-(12, 1, 3500000, '2022-12-01 00:40:27');
+(12, 1, 3500000, '2022-12-01 00:40:27'),
+(13, 0, 1500000, '2022-10-27 03:30:56');
 
 -- --------------------------------------------------------
 
@@ -115,6 +148,20 @@ INSERT INTO `tbl_user` (`id_user`, `nama`, `email`, `password`, `tempat_lahir`, 
 --
 
 --
+-- Indeks untuk tabel `konfirmasi_pinjam`
+--
+ALTER TABLE `konfirmasi_pinjam`
+  ADD PRIMARY KEY (`id_konfirmasi_pinjam`),
+  ADD KEY `id_pinjam` (`id_pinjam`);
+
+--
+-- Indeks untuk tabel `tbl_pengembalian`
+--
+ALTER TABLE `tbl_pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`),
+  ADD KEY `id_konfirmasi_peminjaman` (`id_konfirmasi_peminjaman`);
+
+--
 -- Indeks untuk tabel `tbl_pinjam`
 --
 ALTER TABLE `tbl_pinjam`
@@ -139,6 +186,18 @@ ALTER TABLE `tbl_user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `konfirmasi_pinjam`
+--
+ALTER TABLE `konfirmasi_pinjam`
+  MODIFY `id_konfirmasi_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_pengembalian`
+--
+ALTER TABLE `tbl_pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `tbl_pinjam`
 --
 ALTER TABLE `tbl_pinjam`
@@ -148,7 +207,7 @@ ALTER TABLE `tbl_pinjam`
 -- AUTO_INCREMENT untuk tabel `tbl_simpan`
 --
 ALTER TABLE `tbl_simpan`
-  MODIFY `id_simpan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_simpan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
