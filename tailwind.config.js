9e30fe89f70e7a78c533a71c976895a78de1f563
aufaramdhn <ramadhanaufa3@gmail.com>
2022-12-09 17:50:05 +0700

    update all

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tes/*.{js,php,html}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
